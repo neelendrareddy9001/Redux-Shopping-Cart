6b9886d1f8b3bb32e99c34910be307150caee3d2
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-02 11:54:46 +0100

    Update CartItem.js

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { ChevronDown, ChevronUp } from '../icons';
+import { useDispatch} from 'react-redux';
+import {removeItem, increase, decrease } from '../features/cart/cartSlice';
 
 const CartItem = ({id,img,title,price,amount}) => {
+  const dispatch = useDispatch();
   return (
     <article className='cart-item'>
       <img src={img} alt={title} />
       <div>
          <h4>{title}</h4>
          <h4 className='item-price'>${price}</h4> 
-         <button className='remove-btn'>Remove</button>
+         <button className='remove-btn' onClick={() => {
+              dispatch(removeItem(id));
+            }}
+         >Remove</button>
       </div>
       <div>
-        <button className='amoutn-btn'>
+        <button className='amoutn-btn' onClick={() => dispatch(increase(id))}> 
           <ChevronUp/>
         </button>
         <p className='amount'>{amount}</p>
