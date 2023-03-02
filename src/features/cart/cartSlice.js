8a72e6bd9099ad6b4ed002f5fd79cb933a9c5598
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-02 11:50:58 +0100

    Update cartSlice.js

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -13,11 +13,24 @@ const cartSlice = createSlice({
     initialState,
     reducers : {
        //state.cartItems = [];
-        return {cartItems : []};
-    }
+        //return {cartItems : []};
+    },
+    removeItem  : (state, action) => {
+            //console.log(action);
+            const itemId = action.payload;
+            state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+        },
+        increase : (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount + 1;
+        },
+        decrease : (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount - 1;
+        },
 });
 
 //console.log(cartSlice);
-export const {clearCart} = cartSlice.actions;
+export const {clearCart, increase,decrease} = cartSlice.actions;
 
 export default cartSlice.reducer;
